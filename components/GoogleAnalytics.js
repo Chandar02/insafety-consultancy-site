c8b7538eaa4a9cc4e1dc8dda250bb03d7c5ca9fa
Chandar02 <chandarsingh@live.com>
2025-11-17 15:26:45 +0530

    fix: Explicitly set GA preload link in layout.js to fix Next.js caching issue

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,34 +1,37 @@
 // components/GoogleAnalytics.js
-"use client"; // This component needs to run in the client environment
+"use client"; 
 
 import Script from 'next/script';
 
 const GoogleAnalytics = ({ measurementId }) => {
-  if (!measurementId) return null;
+  if (!measurementId || measurementId === 'G-XXXXXXXXXX') return null; // Added check for fallback value
 
-  return (
-    <>
-      {/* Google Tag Manager/Analytics Base Script */}
-      <Script
-        strategy="afterInteractive" // Loads after the page is interactive (best for performance)
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-      />
-      
-      {/* Analytics Initialization Script */}
-      <Script 
-        id="google-analytics-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${measurementId}');
-          `,
-        }}
-      />
-    </>
-  );
+  return (
+    <>
+      {/* Change strategy from 'afterInteractive' to 'lazyOnload' or 'worker' 
+          We use lazyOnload to ensure it is loaded, but doesn't interfere with the head/preload logic. 
+          Alternatively, you can just remove this Script since the link is preloaded and the init script covers it.
+        */}
+      <Script
+        strategy="lazyOnload" 
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+      />
+      
+      {/* Analytics Initialization Script (KEEP AS IS) */}
+      <Script 
+        id="google-analytics-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${measurementId}');
+          `,
+        }}
+      />
+    </>
+  );
 };
 
 export default GoogleAnalytics;
