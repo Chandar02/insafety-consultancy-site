58f511cb8f267980a4329a872255f3373182b835
Chandar02 <chandarsingh@live.com>
2026-04-07 17:25:42 +0530

    Fix hamburger contact us button

--- components/Header.js
+++ components/Header.js
@@ -105,11 +105,12 @@ export default function Header() {
                         );
                     })}
 
-                    {/* CONTACT BUTTON */}
+                   {/* CONTACT BUTTON */}
                     <li className="nav-item contact-button-wrapper">
                         <a
                             href="#contact-form-anchor"
                             className="contact-button"
+                            style={{ position: "relative", zIndex: 9999 }}
                             onClick={(e) => {
                                 e.preventDefault();
                                 handleNavClick("contact-form-anchor");
